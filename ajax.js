dce7b23aebd2df1b5310fde4e2231c0f4656b5b0
Maru10H <gruposysta@gmail.com>
2021-11-27 21:26:15 -0300

    configurando formulario

--- ajax.js
+++ ajax.js
@@ -1,32 +1,23 @@
-const $nombre = document.querySelector("#nombre"), 
-    $celular = document.querySelector("#celular"),// los 3 input
-    $correo = document.querySelector("#correo"),
-    $mensaje = document.querySelector("#mensaje"),
-    $btnEnviar = document.querySelector("#botonSubmit"), // El botón que envía el formulario
-    $respuesta = document.querySelector("#msjEnviado"); // el div que muestra mensajes
+const form = document.getElementById('formulario')
+const btnEnviar = document.getElementById("botonSubmit")
+const respuesta = document.getElementById("msjEnviado")
 
-// Agregar listener al botón
-$btnEnviar.addEventListener("click", (e) => {
+
+btnEnviar.addEventListener("click", (e) => {
     e.preventDefault();
 	verificar();
-
-	const datos = {
-        nombre: $nombre.value,
-        celular: $celular.value,
-        correo: $correo.value,
-        mensaje: $mensaje.value,
-        
-    };
-    // Codificarlo como JSON
-    const datosCodificados = JSON.stringify(datos);
-    // Enviarlos
-    fetch("pruebas1.php", {
-            method: "POST", // Enviar por POST
-            body: datosCodificados, // En el cuerpo van los datos
-        })
-        .then(respuestaCodificada => respuestaCodificada.json()) // Decodificar JSON que nos responde PHP
-        .then(respuestaDecodificada => {
-            // Aquí ya tenemos la respuesta lista para ser procesada
-            $respuesta.textContent = respuestaDecodificada;
-        });
+	
+	let formData = new FormData(form);
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", form.action, true);
+	xhr.onload = function(e) {
+		let response = JSON.parse(xhr.response);
+		if (xhr.status === 200) {
+		respuesta.innerHTML = "Mensaje enviado!";
+		} else {
+		respuesta.innerHTML = "Hubo un error: " + response.error;
+		}
+	};
+	xhr.send(formData); 
 });
+	
